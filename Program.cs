29c3f2579afc81c639c4c6da9e1d8e6c70b1f88f
Snake <stop@mail.ru>
2023-07-30 19:14:48 +0300

    task21

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите пятизначное число:");
-string number=Console.ReadLine();
-if(number.Length!=5) Console.WriteLine($"Число {number} не пятизначное.");
-//int num=new Random().Next(10000,99999);
-else{
-    int num =int.Parse(number);
-    if(num/10000==num%10 && (num/1000)%10==(num/10)%10) Console.WriteLine($"Число {num} палиндром.");
-    else Console.WriteLine($"Число {num} не палиндром.");}
-
+Console.WriteLine("Введите координаты первой точки через пробел:");
+string a=Console.ReadLine();
+string[] a1=a.Split(' ');
+Console.WriteLine("Введите координаты второй точки через пробел:");
+string b=Console.ReadLine();
+string[] b1=b.Split(' ');
+Console.WriteLine($"Координаты первой точки: ({a1[0]};{a1[1]};{a1[2]}).");
+Console.WriteLine($"Координаты второй точки: ({b1[0]};{b1[1]};{b1[2]}).");
+int l1=int.Parse(a1[0])-int.Parse(b1[0]);
+int l2=int.Parse(a1[1])-int.Parse(b1[1]);
+int l3=int.Parse(a1[2])-int.Parse(b1[2]);
+double dist=Math.Sqrt(Math.Pow(l1,2)+Math.Pow(l2,2)+Math.Pow(l3,2));
+Console.WriteLine($"Расстояние между точками ({a1[0]};{a1[1]};{a1[2]}) и ({b1[0]};{b1[1]};{b1[2]}) равно {dist:f2}");
 
