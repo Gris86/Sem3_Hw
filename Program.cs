3cae5d851d7b800cc9fe326dc932a29f30bb5768
Snake <stop@mail.ru>
2023-07-30 17:48:28 +0300

    task19_var2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите пятизначное число:");
-string num=Console.ReadLine();
-if(num.Length!=5) Console.WriteLine("Число не пятизначное!");
+string number=Console.ReadLine();
+if(number.Length!=5) Console.WriteLine($"Число {number} не пятизначное.");
+//int num=new Random().Next(10000,99999);
 else{
-    if(num[0]==num[4] && num[1]==num[3]) Console.WriteLine($"Число {num} палиндром.");
-    else Console.WriteLine($"Число {num} не палиндром.");
-}
+    int num =int.Parse(number);
+    if(num/10000==num%10 && (num/1000)%10==(num/10)%10) Console.WriteLine($"Число {num} палиндром.");
+    else Console.WriteLine($"Число {num} не палиндром.");}
+
 
